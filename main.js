ef19e4e14ccdd333c36b97470bba5c8d7d83617d
dashadoroshenko <dasha4281@gmail.com>
2021-03-16 19:52:56 +0200

    first

---
+++ main.js
@@ -0,0 +1,18 @@
+/*     zavd1
+const min = require('min').min;
+const abs = require('abs').abs;
+const summ = require('summ').summ;
+
+min([5, 15,101, 0]);
+console.log('  ');
+abs(-2);
+console.log('  ');
+summ(200,60,3);
+*/
+
+const {Mathjs} = require("math");
+Mathjs.min([5, 15,101, 0]);
+console.log('  ');
+Mathjs.abs(-2);
+console.log('  ');
+Mathjs.summ(200,60,3);
